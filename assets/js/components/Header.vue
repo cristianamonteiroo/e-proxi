313e98b266cf0023db6146fcb95d156e14361f57
Cristiana <cristiana.raquel1616@hotmail.com>
2021-04-08 20:40:52 +0200

    'Project-Cloud'

---
+++ assets/js/components/Header.vue
@@ -0,0 +1,11 @@
+<template>
+  <div>
+    <h1>Ici par example l'header page</h1>
+  </div>
+</template>
+
+<script>
+export default {
+ name: 'Header',
+};
+</script>
---
+++ assets/js/views/App.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <Header />
+    <FirstPage />
+    <SecondPage />
+  </div>
+</template>
+
+<script>
+import FirstPage from '../components/FirstPage.vue'
+import SecondPage from '../components/SecondPage.vue'
+import Header from '../components/Header.vue'
+
+export default {
+ name: 'App',
+  components: {
+    FirstPage,
+    SecondPage, 
+    Header
+  }
+};
+</script>
+
+<style>
+.center {
+  text-align: center;
+}
+</style>
